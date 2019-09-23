06afb684156ff4e1efb2bdbc0cda4e583ce9177c
Jeffrey Joseph <jjrzacal@outlook.com>
2019-09-23 13:16:12 -0700

    final?

--- Prob43_Sub_string_divisibility/Prob43_Sub_string_divisibility.cpp
+++ Prob43_Sub_string_divisibility/Prob43_Sub_string_divisibility.cpp
@@ -20,28 +20,28 @@ void swap(char *x, char *y)
 	*y = temp;
 }
 
-void permute(char *a, int l, int r, unsigned long long int *bigsum)
+void jumble(char *s, int l, int r, unsigned long long int *bigsum)
 {
 	int i, digit;
 	unsigned long long int sum, n;
 
 	if (l == r)
 	{
-		if (((a[1] - 48) * 100 + (a[2] - 48) * 10 + (a[3] - 48)) % 2 == 0
-			&& ((a[2] - 48) * 100 + (a[3] - 48) * 10 + (a[4] - 48)) % 3 == 0
-			&& ((a[3] - 48) * 100 + (a[4] - 48) * 10 + (a[5] - 48)) % 5 == 0
-			&& ((a[4] - 48) * 100 + (a[5] - 48) * 10 + (a[6] - 48)) % 7 == 0
-			&& ((a[5] - 48) * 100 + (a[6] - 48) * 10 + (a[7] - 48)) % 11 == 0
-			&& ((a[6] - 48) * 100 + (a[7] - 48) * 10 + (a[8] - 48)) % 13 == 0
-			&& ((a[7] - 48) * 100 + (a[8] - 48) * 10 + (a[9] - 48)) % 17 == 0
+		if (((s[1] - 48) * 100 + (s[2] - 48) * 10 + (s[3] - 48)) % 2 == 0
+			&& ((s[2] - 48) * 100 + (s[3] - 48) * 10 + (s[4] - 48)) % 3 == 0
+			&& ((s[3] - 48) * 100 + (s[4] - 48) * 10 + (s[5] - 48)) % 5 == 0
+			&& ((s[4] - 48) * 100 + (s[5] - 48) * 10 + (s[6] - 48)) % 7 == 0
+			&& ((s[5] - 48) * 100 + (s[6] - 48) * 10 + (s[7] - 48)) % 11 == 0
+			&& ((s[6] - 48) * 100 + (s[7] - 48) * 10 + (s[8] - 48)) % 13 == 0
+			&& ((s[7] - 48) * 100 + (s[8] - 48) * 10 + (s[9] - 48)) % 17 == 0
 			)
 		{
 			sum = 0;
 			for (i = 0; i < 10; i++)
 			{
-				sum = sum * 10 + a[i] - 48;
+				sum = sum * 10 + s[i] - 48;
 			}		
-			printf("%s %lld\n", a, sum);
+			printf("%s %lld\n", s, sum);
 			*bigsum = *bigsum + sum;
 		}
 			
@@ -50,9 +50,9 @@ void permute(char *a, int l, int r, unsigned long long int *bigsum)
 	{
 		for (i = l; i <= r; i++)
 		{
-			swap((a + l), (a + i));
-			permute(a, l + 1, r, bigsum);
-			swap((a + l), (a + i)); //backtrack 
+			swap((s + l), (s + i));
+			jumble(s, l + 1, r, bigsum);
+			swap((s + l), (s + i)); //backtrack 
 		}
 	}
 }
@@ -60,9 +60,9 @@ void permute(char *a, int l, int r, unsigned long long int *bigsum)
 void main()
 {
 	char str[] = "0123456789";
-	int n = strlen(str);
 	unsigned long long int bigsum = 0;
-	permute(str, 0, n - 1, &bigsum);
+	//start and end index
+	jumble(str, 0, 9, &bigsum);
 	printf("\n%lld", bigsum);
 }
 
